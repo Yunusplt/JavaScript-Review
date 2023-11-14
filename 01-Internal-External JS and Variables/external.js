00f6a8919d46e55f48b45ddb22149a4d46ec9cc6
yunus <yunuspolat2528@gmail.com>
2023-11-14 16:08:24 +0100

    first commit

---
+++ 01-Internal-External JS and Variables/external.js
@@ -0,0 +1,11 @@
+console.log("I'am external JavaScript");
+
+
+
+const age=25
+console.log(age);
+//! age = 30                    man kann ihr Wert später nicht geändert werden, wenn die Variable zuerst mit const definiert wurde.
+
+let job = "Frontend Developer"
+job = "Backend Developer"       //! man kann ihr Wert später geändert werden, wenn die Variable zuerst mit let definiert wurde.
+console.log(job)
